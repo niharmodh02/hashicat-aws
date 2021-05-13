0eea8c71aa97967b06c39ac381ce77786583cb17
niharmodh02 <84156319+niharmodh02@users.noreply.github.com>
2021-05-13 18:59:27 -0400

    Update S3-bucket.tf

--- S3-bucket.tf
+++ S3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/Nihar/s3-bucket/aws"
   version = "1.15.0"
  
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-njnnjsbjbj-000"
   acl    = "private"
 
 }
